f292eede0f53a73533177e76148e6d0f0ca510a2
Emily Shepherd <ams2g11@ecs.soton.ac.uk>
2012-10-24 15:29:07 +0000

    ADD: Test for mydispose

--- test.c
+++ test.c
@@ -5,5 +5,12 @@ int main()
 {
 	int *array = malloc(500 * sizeof(int));
 
-	printf("Myinit: %d", myinit(array, 500));
+	printf("Myinit: %d \n", myinit(array, 500));
+
+	printf("Is the array initialised? %d\n", mydispose(array));
+
+	//Test with another random array to make sure we don't have false
+	//positives
+	int *otherArray = malloc(10 * sizeof(int));
+	printf("Is the other array wrong? %d\n", mydispose(otherArray));
 }
