fd51fee3de2f1c91e5393b69ac3c8ab6d89f0fba
Emily Shepherd <ams2g11@ecs.soton.ac.uk>
2012-10-25 23:41:36 +0000

    ADD: gui_show_array() which displays the array contents, split into blocks with the header information interpreted. This is NOT a full replacement to show_array() as this function will only work correctly when the heap isn't corrupted. show_array() will be more useful to debug fundimental issues. gui_show_array() is more useful to debug which blocks we are allocating memory to

---
+++ GUIview.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+//Steal the inner function used by our mymalloc and myfree functions
+int inc_header(int *, int *);
+
+void gui_show_array(int *array)
+{
+    printf("Total memory space: %d\n\n", array[0]);
+    
+    int curheader = 1;
+    
+    printf("+------------------------------------+\n");
+    
+    while (1)
+    {
+        int size;
+        
+        if (array[curheader] > 0)
+        {
+            size = array[curheader];
+            printf("| Free block, size: %d\n", size);
+        }
+        else
+        {
+            size = -1 * array[curheader];
+            printf("| Used block, size: %d\n", size);
+        }
+        
+        for (int i = curheader + 1; i < curheader + size + 1; i++)
+        {
+            if (array[curheader] > 0)
+            {
+                printf("| (%d)\n", array[i]);
+            }
+            else
+            {
+                printf("| %d\n", array[i]);
+            }
+        }
+        
+        printf("+------------------------------------+\n");
+
+        if (!inc_header(array, &curheader)) return;
+    }
+}
--- test.c
+++ test.c
@@ -5,6 +5,7 @@ int myinit(int *, int);
 int* mymalloc(int *, int);
 int mydispose(int *);
 int myfree(int *, int *);
+void gui_show_array(int *);
 
 int *array;
 
@@ -74,12 +75,31 @@ int main()
 {
 	init(50);
   
-	malloc_test(array, 50, 101);
-	malloc_test(array, 49, 101);
-	malloc_test(array, 48, 101);
-	malloc_test(array, 47, 101);
+	int *p1 = malloc_test(array, 5, 101);
+	int *p2 = malloc_test(array, 8, 108);
+	int *p3 = malloc_test(array, 3, 103);
+	int *p4 = malloc_test(array, 10, 110);
 	
-	show_array();
+	myfree(array, p2);
+	myfree(array, p3);
+	
+	p2 = malloc_test(array, 5, 105);
+	p3 = malloc_test(array, 18, 118);
+	
+	//I expect this to fill the space of size 6. Because using
+	//this space leaves one int left over, which can't be used
+	//for a header and data. mymalloc() should assign 6  bytes
+	//to us anyway, to avoid that byte being lost.
+	//The down side of this is that this byte won't be recovered
+	//until this block is freed, even if the next block is freed.
+	malloc_test(array, 5, 666);
+	
+	//I expect this to fail
+	malloc_test(array, 1, 777);
+	
+	gui_show_array(array);
+	
+	if (*p1 + *p2 + *p3 + *p4) {}
   
 	return 0;
 }
