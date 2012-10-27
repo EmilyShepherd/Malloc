91b4e3f85805c8175addbc44b6733b244e9fa029
Emily Shepherd <ams2g11@ecs.soton.ac.uk>
2012-10-27 01:14:37 +0000

    REF: Fixed comments wrapped too early

--- mymalloc.c
+++ mymalloc.c
@@ -38,9 +38,9 @@ int decode_header(int *header, int *blocksize, int *headersize)
     int free            = 0;
     int bytes           = 0;
     
-    //The 7th bit has in the first byte is the free flag,
-    //so check that. If it's 1, set it to 0 so it doesn't
-    //get used as part of the value by the code below.
+    //The 7th bit has in the first byte is the free flag, so check that.
+    //If it's 1, set it to 0 so it doesn't get used as part of the value
+    //by the code below.
     if ((*byte & 0x40) == 0x40)
     {
         free   = 1;
@@ -49,8 +49,7 @@ int decode_header(int *header, int *blocksize, int *headersize)
     
     while (1)
     {
-        //Shift the value up 7 bits and put the next 7 bits
-        //in after it.
+        //Shift the value up 7 bits and put the next 7 bits in after it.
         usize = (usize << 7) + (*byte & 0x7F); //0111 1111
         
         bytes++;
@@ -79,8 +78,8 @@ int encode_header(int *iarray, int free, int size)
     unsigned int usize    = (unsigned int)size;
     int pos               = 0;
     
-    //Split the number into 5 bytes, each containing
-    //7 bits of the value.
+    //Split the number into 5 bytes, each containing 7 bits of the
+    //value.
     unsigned char byte[5];
     byte[0] = usize >> 28 & 0x0F;
     byte[1] = usize >> 21 & 0x7F;
@@ -88,28 +87,27 @@ int encode_header(int *iarray, int free, int size)
     byte[3] = usize >> 7  & 0x7F;
     byte[4] = usize       & 0x7F;
     
-    //Loop through the bytes, looking for the first
-    //one that's used - so we need not save the higher
-    //unused bytes.
+    //Loop through the bytes, looking for the first one that's used - so
+    //we need not save the higher unused bytes.
     for (pos = 0; byte[pos] == 0; pos++);
     
-    //Check if this byte uses its 7th bit. If it does
-    //we'll need to create a new byte, because we need
-    //the 7th bit of the first byte for the sign.
+    //Check if this byte uses its 7th bit. If it does we'll need to
+    //create a new byte, because we need the 7th bit of the first byte
+    //for the sign.
     if ((byte[pos] & 0x40) == 0x40)
     {
         pos--;
     }
     
-    //If we want to mark this header's block as free, save
-    //1 into the 7th bit of the first byte
+    //If we want to mark this header's block as free, save 1 into the
+    //7th bit of the first byte
     if (free)
     {
         byte[pos] = byte[pos] | 0x40; //0100 0000
     }
     
-    //Save all except the last byte, with the MSB bit
-    //set to 1 to show there are more bytes to follow
+    //Save all except the last byte, with the MSB bit set to 1 to show
+    //there are more bytes to follow
     for (int i = pos; i < 4; i++)
     {
         *barray = byte[i] | 0x80; //1000 0000
@@ -117,8 +115,8 @@ int encode_header(int *iarray, int free, int size)
     }
     
     //Save the last byte
-    //The 8th bit must be 0, but we know it is, because of
-    //the 0x7F mask we used above.
+    //The 8th bit must be 0, but we know it is, because of the 0x7F mask
+    //we used above.
     *barray = byte[4];
     
     return 5 - pos;
