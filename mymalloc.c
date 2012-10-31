35506f716a3f3e164b1269b22e43bf77d597d258
Emily Shepherd <ams2g11@ecs.soton.ac.uk>
2012-10-31 13:56:28 +0000

    FIX: myinit() end headers placed 1 byte out

--- mymalloc.c
+++ mymalloc.c
@@ -1,4 +1,5 @@
 #include "mymalloc.h"
+#include <stdio.h>
 
 /*
  * @depreciated
@@ -80,7 +81,7 @@ int decode_header(int *header, int *blocksize, int *headersize)
  * Encodes the given free flag and size into the the given array,
  * returning how many bytes it used.
  */
-int encode_header(char *iarray, int free, int size)
+int encode_header(int *iarray, int free, int size)
 {
     unsigned char *barray = (unsigned char *)iarray;
     unsigned int usize    = (unsigned int)size;
@@ -142,24 +143,27 @@ int myinit(int *array, int size)
     *array = size;
     
 
-    int header2size = encode_header(barray + 4, 1, size - 2);
+    int header2size = encode_header((int *)(barray + 4), 1, size - 2);
+
+printf("%d -> %d", size, header2size);
+
     barray  += 1;
 
     if (header2size == 3)
     {
-        barray += 4*size - 1;
+        barray += 4*size - 2;
         *barray = 64;
     }
     else if (header2size == 2)
     {
-        barray += 4*size - 2;
+        barray += 4*size - 3;
         *barray = (char) 0xC0;
         barray++;
         *barray = 0;
     }
     else if (header2size == 1)
     {
-        barray += 4*size - 3;
+        barray += 4*size - 4;
         *barray = (char) 192;
         barray++;
         *barray = 0;
