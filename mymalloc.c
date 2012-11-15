39efe275c061d34c1d814682e7060de9eaa8fbe0
Alex Hutson <ah16g11@ecs.soton.ac.uk>
2012-11-15 14:04:03 +0000

    FIX: multithreaded functions named incorrectly

--- mymalloc.c
+++ mymalloc.c
@@ -132,9 +132,9 @@ int mydispose(int *array)
 	return 0; 
 }
 
-int mt_myinit(int *array, int size){ return 0;}
-int* mt_mymalloc(int *array, int size){ return (int*) 0; }
-int mt_myfree(int *array, int *block){ return 0;}
-int mt_mydispose(int *array){ return 0;}
+int myinit_mt(int *array, int size){ return 0;}
+int* mymalloc_mt(int *array, int size){ return (int*) 0; }
+int myfree_mt(int *array, int *block){ return 0;}
+int mydispose_mt(int *array){ return 0;}
 
 
