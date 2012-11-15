101ddac45a39a520ab58a9328540d027af152450
Emily Shepherd <ams2g11@ecs.soton.ac.uk>
2012-11-15 13:59:44 +0000

    ADD: Updated mymalloc.h with the multithreaded hints

--- mymalloc.h
+++ mymalloc.h
@@ -2,3 +2,8 @@ int myinit(int *, int);
 int* mymalloc(int *, int);
 int mydispose(int *);
 int myfree(int *, int *);
+
+int mt_myinit(int *, int);
+int* mt_mymalloc(int *, int);
+int mt_mydispose(int *);
+int mt_myfree(int *, int *);
