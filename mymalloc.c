d78dfdccd91e8234516ad095bf6ec4fef5668e4b
Emily Shepherd <ams2g11@ecs.soton.ac.uk>
2012-11-20 15:12:23 +0000

    FMT: Fixed line length and added new comments

--- mymalloc.c
+++ mymalloc.c
@@ -1,16 +1,41 @@
 #include "mymalloc.h"
 #include <pthread.h>
+/*
+ * Alexander Shepherd ams2g11
+ * Alex Hutson ah16g11
+ *
+ * Our solution uses a single integer as a header for each block of
+ * allocated data. The size of each block is the absolute value of the
+ * integer. Positive integer headers indicate that the block is free,
+ * while negative integer headers indicate used blocks. The first
+ * integer in the array is used to store the total size of the array,
+ * and the second space is used as the first header. Initially this
+ * header represents a block which takes up the remaining space of the
+ * array (total size - 2). When space is allocated, the free block is
+ * split into the allocated block of required size and a new free block
+ * bellow it, with the exception of the case when the free block is the
+ * required size + 1, in this case, the extra integer is allocated to
+ * the block as well, because it can't be used for anything. We've
+ * improved this by using best fit, instead of first fit. When freeing a
+ * block, it will be combined with other free blocks either side of it,
+ * if such blocks exist. My free has to iterate across the entire array
+ * to confirm that a given pointer is the start of a valid block.
+ */
 
+/*
+ * This mutex is used for the whole solution when multithreaded.
+ */
 static pthread_mutex_t mutex;
 
-
-// ah16g11 & ams2g11
-// Our solution uses a single integer as a header for each block of allocated data. The size of each block is the absolute value of the integer. Positive integer headers indicate that the block is free, while negative integer headers indicate used blocks. The first integer in the array is used to store the total size of the array, and the second space is used as the first header. Initially this header represents a block which takes up the remaining space of the array (total size - 2). When space is allocated, the free block is split into the allocated block of required size and a new free block bellow it, with the exception of the case when the free block is the required size + 1, in this case, the extra integer is allocated to the block as well, because it can't be used for anything. We've improved this by using best fit, instead of first fit. When freeing a block, it will be combined with other free blocks either side of it, if such blocks exist. My free has to iterate across the entire array to confirm that a given pointer is the start of a valid block.
-
-
+/*
+ * Increments the given header index to point to the next header
+ *
+ * Returns 1 on success or 0 on failure (when the end of the array has
+ * been reached)
+ */
 static int inc_header(int *array, int *curheader)
 {
-    // Jump to the next header
+    //Jump to the next header
     if (array[*curheader] < 0)
     {
         *curheader += -1*array[*curheader] + 1;
@@ -20,7 +45,7 @@ static int inc_header(int *array, int *curheader)
         *curheader += array[*curheader] + 1;
     }
 
-    // If header is outside of the array, there are no more blocks
+    //If header is outside of the array, there are no more blocks
     if (*curheader >= array[0])
     {
         return 0;
@@ -31,23 +56,38 @@ static int inc_header(int *array, int *curheader)
     }
 }
 
+/*
+ * Intialises the array by saving the total size in the first space, and
+ * creating the first header which is for a free block taking up the
+ * whole of the remaining space.
+ *
+ * Returns 1 on success or 0 on failure (if the given array is too small
+ * to use)
+ */
 int myinit(int *array, int size)
 {
-    // Smallest size available chosen to be usable, otherwise return 0
+    //Smallest size available chosen to be usable, otherwise return 0
+    //1 int for total array size
+    //1 int for block header
+    //1 int for content
     if (size < 3) return 0;
 
-    // Put size of array into first position
+    //Put size of array into first position
     array[0] = size;
 
-    // Make the rest of the array into a free block
+    //Make the rest of the array into a free block
     array[1] = size - 2;
 
     return 1;
 }
 
+/*
+ * Returns a pointer to a block within the array of the given size if
+ * one is availiabe. (int *)0 otherwise.
+ */
 int* mymalloc(int *array, int size)
 {
-    // Validates given size
+    //Validates given size is not garbage (<= 0) or too big to use
     if (size <= 0 || size >= array[0]) return (int *)0;
     
     int curheader   = 1;
@@ -66,22 +106,27 @@ int* mymalloc(int *array, int size)
                 array[curheader] *= -1;
                 return &array[curheader+1];
             }
-            // Int space will be wasted
+            //If this space is used, an integer will be wasted at the
+            //end. We would rather use something else, but save it
+            //way incase no other space exists
             else if (size + 1 == array[curheader])
             {
                 wasteheader = curheader;
             }
-            // Space is bigger than needed
+            //Space is bigger than needed, save it if it's the smallest
+            //too big block
             else if (array[curheader] < bestsize)
             {
                 bestheader = curheader;
                 bestsize   = array[curheader];
             }
         }
-
+        
         if (!inc_header(array, &curheader)) break;
     } 
-
+    
+    //We have a space saved, use it and break it into a free block after
+    //the allocated size
     if (bestheader != 0)
     {
         //Allocate space, then put header after data for new section of
@@ -91,7 +136,7 @@ int* mymalloc(int *array, int size)
 
         return &array[bestheader+1];
     }
-    // If only space avaialable will waste an int
+    //If only space avaialable will waste an int
     else if (wasteheader != 0)
     {
         array[wasteheader] *= -1;
@@ -102,8 +147,16 @@ int* mymalloc(int *array, int size)
     return (int *)0;
 }
 
+/*
+ * Frees the given block
+ *
+ * Returns 1 on success or 0 on failure (when the given pointer is not
+ * the start of a used block within the array)
+ */
 int myfree(int *array, int *block)
 {
+    //If the block pointer is outside the bounds of the array, its
+    //obviously wrong
     if (block <= array || block >= array + array[0]) return 0;
     
     int curheader  = 1;
@@ -142,7 +195,10 @@ int myfree(int *array, int *block)
 
         //Save this header and move unto the next one
         lastheader = curheader;
-        if (!inc_header(array, &curheader) || &array[curheader] > block) return 0;
+        if (!inc_header(array, &curheader) || &array[curheader] > block)
+        {
+            return 0;
+        }
     }
 }
 
